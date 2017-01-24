a9b5df70db749d2ef38abbb68dd8fa1c8b58b00a
Tamas Varadi <1997tamas.varadi@gmail.com>
2017-01-24 21:02:28 +0100

    changed folder name to godot-mysql-module in register_types.cpp/.h

--- register_types.cpp
+++ register_types.cpp
@@ -11,12 +11,12 @@
 #include "cppconn/resultset.h"
 #include "cppconn/statement.h"
 
-void register_mysql_types() {
+void register_godot-mysql-module_types() {
 
         ObjectTypeDB::register_type<MySQL>();
 }
 
-void unregister_mysql_types() {
+void unregister_godot-mysql-module_types() {
    //nothing to do here
 }
 
--- register_types.h
+++ register_types.h
@@ -1,5 +1,5 @@
 /* register_types.h */
 
-void register_mysql_types();
-void unregister_mysql_types();
+void register_godot-mysql-module_types();
+void unregister_godot-mysql-module_types();
 /* yes, the word in the middle must be the same as the module folder name */
